6d6ea97dd1a43b82f0043d3348be79c9a63bd265
Jeffrey Reij <contact@jreij.info>
2026-02-03 04:33:31 +0100

    Update test.ts

--- src/lib/src/test.ts
+++ src/lib/src/test.ts
@@ -3,6 +3,6 @@ import SpotifyPatcher from "./SpotifyPatcher";
 (async () => {
     const patcher = new SpotifyPatcher();
     console.log(patcher.config_dir_path);
-    console.log(await patcher.getConfig())
-    console.log(await patcher.extractSpotifyApps())
+    await patcher.extractSpotifyApps();
+    await patcher.patchSpotifyApps();
 })();
